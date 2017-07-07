d0a96f54721355f20d5b444d63a5155b12646abb
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-07 16:24:57 -0400

    Math input project

---
+++ MathInput/MathInput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MathInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the first # to add");
+            string firstNum = Console.ReadLine();
+
+            Console.WriteLine("Give me the second # you want to add");
+            string secondNum = Console.ReadLine();
+            //concatilation
+            int x = Convert.ToInt32(firstNum);
+            int y = Convert.ToInt32(secondNum);
+            int sum = x + y;
+            Console.WriteLine($"Your first number was {firstNum} and your second number was {secondNum}, the sum is {sum}.");
+
+
+
+
+
+
+
+
+        }
+    }
+}
