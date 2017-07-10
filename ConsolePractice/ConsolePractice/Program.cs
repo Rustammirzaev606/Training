f7a0c8714513371a4681ee6de714b13a37042a3c
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-10 15:36:27 -0400

    dude

---
+++ ConsolePractice/ConsolePractice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string dude = Console.ReadLine();
+
+            //Console.WriteLine($"My name is {dude}");
+            //Console.ReadKey();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}! How old are you?");
+            string age = Console.ReadLine();
+            string ageComparison;
+            if (Convert.ToInt32(age) < 20)
+            {
+
+                ageComparison = "teenager";
+            }
+            else if (Convert.ToInt32(age) > 20 && Convert.ToInt32(age) < 55)
+
+            {
+                ageComparison = "young man";
+            }
+
+            else
+            {
+                ageComparison = "grandpa";
+            }
+            Console.WriteLine($"Hi {name}. You are {age} years old {ageComparison}.");
+
+            Console.ReadKey();
+        }  
+    }
+}
+
