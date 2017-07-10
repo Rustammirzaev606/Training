7d81d4b225917ddcbabbfa3c40849390439d0a85
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-10 09:44:18 -0400

    meh

---
+++ TryItOut(Chapter4)/TryItOut(Chapter4)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryItOut_Chapter4_
+{
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            orientation myDirection = orientation.north;
+            byte directionByte;
+            string directionString;
+            Console.WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            Console.WriteLine($"string equivalent = {directionString}");
+            Console.ReadKey();
+
+        }
+    }
+}
