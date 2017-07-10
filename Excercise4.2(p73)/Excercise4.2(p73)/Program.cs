d8c8c4a34097b89eb9fd4a36b2b0cee569dc7515
Rustam <Rustam@RUSTAMSPC>
2017-07-09 20:30:48 -0400

    Done!

---
+++ Excercise4.2(p73)/Excercise4.2(p73)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise4._2_p73_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comparison;
+            Console.WriteLine("Enter two values. One of them MUST be less than 10. The other one MUST be greater than 10");
+            Console.WriteLine("Enter a value for var1");
+            int var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for var2");
+            int var2 = Convert.ToInt32(Console.ReadLine());
+            if (var1 > 10 && var2 > 10)
+                Console.WriteLine("Wrong number buddie!");
+            
+            else
+            {
+                if (var1 > 10)
+                    comparison = $"{var1} is greater than 10!";
+                else
+                {
+                    if (var2 > 10)
+                        comparison = $"{var2} is greater than 10!";
+                    else
+
+                        comparison = $"both {var1} and {var2} are less than 10!";
+
+                }
+
+
+            }
+            Console.WriteLine($"{comparison}");
+
+            Console.ReadKey();
+        }
+    }
+}
