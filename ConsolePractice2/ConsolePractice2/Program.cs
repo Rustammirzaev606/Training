1365cb3786ca18b75481057611f056bb88a5e1dc
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-10 15:56:26 -0400

    phew

--- ConsolePractice2/ConsolePractice2/Program.cs
+++ ConsolePractice2/ConsolePractice2/Program.cs
@@ -10,6 +10,16 @@ namespace ConsolePractice2
     {
         static void Main(string[] args)
         {
+            //amusement park, when did you last time been there?, did u have fun?
+            Console.WriteLine("What's your favorite amusement park? ");
+            string parkName = Console.ReadLine();
+            Console.WriteLine("When did you last time been there?(Type number of months)");
+            string lastTime = Console.ReadLine();
+            Console.WriteLine("Did you have fun?");
+            string fun = Console.ReadLine();
+                    
+            Console.WriteLine($"Your favorite amusement park is {parkName}, last time you've been there {lastTime} month ago and {fun}, you had a lot of fun. ");
+            Console.ReadKey();
         }
     }
 }
