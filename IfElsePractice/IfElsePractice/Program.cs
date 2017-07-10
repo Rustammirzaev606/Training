af2ad41435882545d2cbf90e338ea7913594af7c
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-10 16:35:31 -0400

    bruh

---
+++ IfElsePractice/IfElsePractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElsePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color");
+            string favColor = Console.ReadLine().ToUpper();
+            string blah = "nice";
+            if (favColor == "RED") 
+            {
+                Console.WriteLine("red is good");
+
+            }
+            else
+            {
+                Console.WriteLine("Whatever!!");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
