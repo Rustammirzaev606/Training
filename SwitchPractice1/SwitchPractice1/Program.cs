89db0c3b008368984575ca24161aeeb56265a19a
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-13 11:42:27 -0400

    aaaaa

---
+++ SwitchPractice1/SwitchPractice1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine().ToLower();
+            int foodLike;
+            //int meh = int.Parse
+
+            int.TryParse(color, out foodLike);
+
+            switch(foodLike)
+            {
+                case 5:
+                    Console.WriteLine("Red is an ok Color");
+                    break;;
+                case 2:
+                    Console.WriteLine("Blue is fine too");
+                    break;
+                default:
+                    Console.WriteLine("We dont understand your answer");
+                    break;
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
