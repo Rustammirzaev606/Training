7a56dd5c9b5d71bdad85728f71bef43aad40ee95
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-18 12:37:44 -0400

    qw

--- FizzBuzz_1/FizzBuzz_1/Program.cs
+++ FizzBuzz_1/FizzBuzz_1/Program.cs
@@ -12,29 +12,30 @@ namespace FizzBuzz_1
         {
             Console.WriteLine("For those who do not know: FizzBuzz is a quite popular childrens game. Counting from 1 to 100, and every time a number is divisible by 3 calling Fizz, every time a number is divisible by 5 calling Buzz and every time a number is divisible by 3 and 5, calling FizzBuzz instead of the number");
             int i = 1;
+            string fb;
             for (i = 1; i <= 100; ++i)
             {
                 
                 
                 while ((i % 3) == 0 && (i % 5) != 0)
                 {
-                    Console.WriteLine("FIZZ");
+                    fb = "FIZZ";
                 }
                 while ((i % 3) !=0 && (i % 5) == 0)
                 {
-                    Console.WriteLine("BUZZ");
+                    fb = "BUZZ";
                 }
                 while ((i % 3) == 0 && (i % 5) == 0)
                 {
-                    Console.WriteLine("FIZZ BUZZ");
+                    fb = "FIZZ BUZZ";
                 }
                 while (((i % 3) != 0 || (i % 5) != 5) || ((i % 3) != 0 && (i % 5) != 0))
                 {
-                    Console.WriteLine(i);
+                    fb = Convert.ToString(i);
                     
 
                 }
-
+                Console.WriteLine(i);
 
 
                 Console.ReadKey();
