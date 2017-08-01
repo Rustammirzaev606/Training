f109578abf39e9617ac74c80579f4f0fdeb45f0a
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-01 10:55:37 -0400

    Peekaboo

---
+++ InheritanceHomework2/InheritanceHomework2/MultyplicationData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceHomework2
+{
+    class MultyplicationData
+    {
+        public void SetX(int y)
+        {
+            x = y;
+        }
+        public void RunTime()
+        {
+            
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.WriteLine($"{x} * {y} = {x * y}");
+            }
+        }
+
+        private double x;
+
+        
+    }
+}
---
+++ InheritanceHomework2/InheritanceHomework2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceHomework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp1;
+            string strTempo;
+            MultyplicationData md = new MultyplicationData();
+            Console.WriteLine($"Enter a number:");
+            strTempo = Console.ReadLine();
+            int.TryParse(strTempo, out temp1);
+            md.SetX(temp1);
+            md.RunTime();
+            Console.ReadLine();
+
+
+        }
+    }
+}
