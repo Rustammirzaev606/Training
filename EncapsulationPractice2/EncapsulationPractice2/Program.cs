12bc8106c27a08d6add1a1c96e125a09f60ecf19
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-02 13:42:55 -0400

    1

---
+++ EncapsulationPractice2/EncapsulationPractice2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee rustam = new Employee();
+            rustam.FirstName = "Rustam";
+            rustam.LastName = "Mirzaev";
+            rustam.IdNumber = 5000;
+            rustam.SocNumber = 1234400;
+            rustam.Tuition = 5000;
+            Program p = new Program();
+            p.DisplayInfo(rustam);
+            
+            Console.ReadLine();
+        }
+
+        public void DisplayInfo(Employee emp)
+        {
+            Console.WriteLine($"My first name is {emp.FirstName}.\nMy last name is {emp.LastName}.\nID Number {emp.IdNumber}.\nSocial Security Number {emp.SocNumber}.\n Tuition is {emp.Tuition}");
+
+        }
+    }
+}
---
+++ InterfacePractice1/InterfacePractice1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice1
+{
+    class Program
+    {
+        public interface IWork
+        {
+            void Printing();
+            void Display();
+
+
+        }
+        public interface IClean
+        {
+            void Shop();
+
+
+
+        }
+        public class Names : IWork, IClean
+        {
+            string coder = "nice";
+
+            public void Printing()
+            {
+
+                Console.WriteLine("I love Lucy");
+            }
+            public void Display()
+            {
+                Console.WriteLine("We are in the Display method");
+            }
+            public void Shop()
+            {
+                Console.WriteLine("shopping shmopping");
+            }
+
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            Names memes = new Names();
+            memes.Display();
+            memes.Printing();
+            memes.Shop();
+
+            Console.ReadLine();
+        }
+
+        public interface IRun
+        {
+            void Sprint();
+        }
+    }
+}
