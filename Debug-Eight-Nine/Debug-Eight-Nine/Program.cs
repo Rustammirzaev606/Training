06e1a66d2da1d4a3bcdf33500ed5be9675f0e157
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-07 14:25:15 -0400

    Asd

---
+++ Debug-Eight-Nine/Debug-Eight-Nine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug_Eight_Nine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DebugEight3 boogi = new DebugEight3();
+            boogi.BugEightThree();
+
+            DebugEight4 googie = new DebugEight4();
+            googie.BugEightFour();
+
+            DebugNine1 goofy = new DebugNine1();
+            goofy.BugNine1();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
