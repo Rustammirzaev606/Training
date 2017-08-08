d354f0cf865c30c6f38a6608904e970f0d4a19b1
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-07 23:21:06 -0400

    asd

---
+++ DebugNineTwo/DebugNineTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugNineTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FixedDebugNine2 frenchToast = new FixedDebugNine2();
+            frenchToast.DebugNineTwo();
+            DebugSeven4 seven4 = new DebugSeven4();
+            seven4.SevenFour();
+            Console.ReadLine();
+        }
+    }
+}
