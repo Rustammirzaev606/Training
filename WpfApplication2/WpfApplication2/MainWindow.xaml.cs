55e8dd8462a8091f6f053ad122a6af0e5d0713f0
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-14 15:42:54 -0400

    Working on that project!

---
+++ WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHats_Click(object sender, RoutedEventArgs e)
+        {
+            Hats hatsCall = new Hats();
+            hatsCall.ShowDialog();
+        }
+
+        private void btnShoes_Click(object sender, RoutedEventArgs e)
+        {
+            shoes shoesCall = new shoes();
+            shoesCall.ShowDialog();
+        }
+
+        private void btnPants_Click(object sender, RoutedEventArgs e)
+        {
+            Pants pantsCall = new Pants();
+            pantsCall.ShowDialog();
+        }
+    }
+}
